1b93db98fe13828598a01776a866ac5ed776b46a
cle@hmc.edu <cle@hmc.edu>
2020-05-03 15:14:19 -0400

    Thorough comments for AlbersMeasFcn.m

--- Extended_KFs/Albers/AlbersMeasFcn.m
+++ Extended_KFs/Albers/AlbersMeasFcn.m
@@ -1,15 +1,13 @@
 function yk = AlbersMeasFcn(xk)
-% Author: CLe
+% Author: Cassidy Le
 % Date: November 21, 2019
-% Summary:  Example measurement function for discrete 
-%           time nonlinear state estimators
+% Summary:  Measurement function for discrete time nonlinear state
+%           estimators. Indicates which states are being "measured".
 % Inputs:   xk = states at time k, x[k]
 % Outputs:  yk = y[k], measurements at time k
-%
-% The measurement is the first state with multiplicative noise
 
 % yk = [xk(1); xk(2); xk(3); xk(4); xk(5); xk(6)];
-% Only send out glucose portion
+% Only send out glucose portion xk(3)
 yk = xk(3);
 
 end
