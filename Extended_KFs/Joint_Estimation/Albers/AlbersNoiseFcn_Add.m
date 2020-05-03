1078b69eb6cdb0d745be16150f84b07873a38257
cle@hmc.edu <cle@hmc.edu>
2020-05-03 17:21:02 -0400

    Thorough comments for AlbersNoiseFcn_Add.m

--- Extended_KFs/Joint_Estimation/Albers/AlbersNoiseFcn_Add.m
+++ Extended_KFs/Joint_Estimation/Albers/AlbersNoiseFcn_Add.m
@@ -1,15 +1,23 @@
 function yk = AlbersNoiseFcn_Add(xk)
 % Author: Cassidy Le
-% Date: March 31, 2019
-% Summary:  Example measurement function for discrete time nonlinear
-%           state estimators with non-additive measurement noise.
+% Date: March 31, 2020
+% Summary:  Measurement function for discrete time nonlinear
+%           state estimators with additive measurement noise.
+%           Indicates which states the EKF algorithm is estimating.
+%           In this case, only one state (glucose) and all 3 parameters.
+%           Fcn is additive b/c EKF function will automatically add noise
+%           so we don't need to add noise in this function.
+%           Just call 'HasAdditiveMeasurementNoise' as true.
 % Inputs:   xk = states at time k, x[k]
-% Note:     No measurement noise input v[k]
 % Outputs:  yk = y[k], measurements at time k
-%
-% Fcn is Add b/c UKF function will automatically add noise
-% So we don't need to add noise in this function
-% Just call 'HasAdditiveMeasurementNoise' as true
+
+% Note:     No measurement noise input v[k] b/c algorithm will be set
+%           to include noise additively by calling
+%           'HasAdditiveMeasurementNoise' as true.
+
+% yk = [xk(1); xk(2); xk(3); (states: glucose/insulin)
+%       xk(4); xk(5); xk(6); (feeding)
+%       xk(7); xk(8); xk(9)]; (parameters)
 % Selecting only glucose xk(3) and parameters
 yk = [xk(3); xk(7); xk(8); xk(9)];
 
