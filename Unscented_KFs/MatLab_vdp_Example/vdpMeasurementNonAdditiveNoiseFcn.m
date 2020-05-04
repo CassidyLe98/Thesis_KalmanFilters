6579c74dcacc10a52c3babe75cfecffce47f4805
cle@hmc.edu <cle@hmc.edu>
2020-05-04 13:21:17 -0400

    Thorough comments for vdpMeasurementNonAdditiveNoiseFcn.m

--- Unscented_KFs/MatLab_vdp_Example/vdpMeasurementNonAdditiveNoiseFcn.m
+++ Unscented_KFs/MatLab_vdp_Example/vdpMeasurementNonAdditiveNoiseFcn.m
@@ -1,26 +1,14 @@
 function yk = vdpMeasurementNonAdditiveNoiseFcn(xk,vk)
-% vdpMeasurementNonAdditiveNoiseFcn Example measurement function for discrete
-% time nonlinear state estimators with non-additive measurement noise.
-%
-% yk = vdpNonAdditiveMeasurementFcn(xk,vk)
-%
-% Inputs:
-%    xk - x[k], states at time k
-%    vk - v[k], measurement noise at time k
-%
-% Outputs:
-%    yk - y[k], measurements at time k
-%
+% Summary:  Example measurement function for discrete time
+%           nonlinear state estimators with non-additive measurement noise.
+% Inputs:   xk - x[k], states at time k
+%           vk - v[k], measurement noise at time k
+% Outputs:  yk - y[k], measurements at time k
+
+% Note: Implementation is from on MATLAB unscentedKalmanFilter example.
+%       Copyright 2016 The MathWorks, Inc.
+%       https://www.mathworks.com/help/control/ref/unscentedkalmanfilter.html
+
 % The measurement is the first state with multiplicative noise
-%
-% See also extendedKalmanFilter, unscentedKalmanFilter
-
-%   Copyright 2016 The MathWorks, Inc.
-
-%#codegen
-
-% The tag %#codegen must be included if you wish to generate code with 
-% MATLAB Coder.
-
 yk = xk(1)*(1+vk);
 end
