f33e68cd645191fb9d550fdb6c099cf58d766a75
cle@hmc.edu <cle@hmc.edu>
2020-05-04 13:20:40 -0400

    Thorough comments for vdp1.m

--- Unscented_KFs/MatLab_vdp_Example/vdp1.m
+++ Unscented_KFs/MatLab_vdp_Example/vdp1.m
@@ -1,10 +1,15 @@
 function dydt = vdp1(t,y)
-%VDP1  Evaluate the van der Pol ODEs for mu = 1
-%
-%   See also ODE113, ODE23, ODE45.
+% Summary:  Evaluate the van der Pol ODEs for mu = 1
+%           See also ODE113, ODE23, ODE45.
+% Inputs:   y = input vector
+% Outputs:  set of DEs
 
-%   Jacek Kierzenka and Lawrence F. Shampine
-%   Copyright 1984-2014 The MathWorks, Inc.
+% Note: Implementation is from on MATLAB unscentedKalmanFilter example.
+%       Jacek Kierzenka and Lawrence F. Shampine
+%       Copyright 1984-2014 The MathWorks, Inc.
+%       https://www.mathworks.com/help/control/ref/unscentedkalmanfilter.html
 
-dydt = [y(2); 
-        (1-y(1)^2)*y(2)-y(1)];
+% Output DE vector consists of velocity y(2) and acceleration
+dydt = [y(2); % velocity
+        (1-y(1)^2)*y(2)-y(1)]; % acceleration
+end
