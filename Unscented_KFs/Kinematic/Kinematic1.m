d6cb315de6df61e351fc751ce7a392b9dd511e90
cle@hmc.edu <cle@hmc.edu>
2020-05-03 20:54:38 -0400

    Thorough comments for Kinematic1.m

--- Unscented_KFs/Kinematic/Kinematic1.m
+++ Unscented_KFs/Kinematic/Kinematic1.m
@@ -1,9 +1,16 @@
 function dydt = Kinematic1(t,y)
-% Authors: LdP and CLe
+% Authors: Lisette de Pillis and Cassidy Le
 % Date: November 12, 2019
-% Summary:  This function is the 2x2 1st order system representing h'' = -g
-%           Comes from the paper by Rhudy, et al AKA "undergrad paper"
+% Summary:  The 2x2 1st order system representing h'' = -g. Evaluates the
+%           kinematics system from the paper by Rhudy, Salguero, and Holappa
+%           CITATION: Rhudy, M.B., Salguero, R.A., & Holappa, K. (2017).
+%           A Kalman Filtering Tutorial for Undergraduate Students.
+%           International Journal of Computer Science & Engineering
+%           Survey, 8, 1-18.
 
-g = 9.81; % Gravity m/s^2
+% Define constant for gravity
+g = 9.81; % gravity units = m/s^2
+
+% Output DE vector consists of velocity y(2) and acceleration (-g)
 dydt = [y(2); -g];
 end
