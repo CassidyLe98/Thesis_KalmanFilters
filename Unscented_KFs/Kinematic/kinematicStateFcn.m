b5e871086362a1a56af440c5bf56972b6960a636
cle@hmc.edu <cle@hmc.edu>
2020-05-03 21:14:01 -0400

    Thorough comments for kinematicStateFcn.m

--- Unscented_KFs/Kinematic/kinematicStateFcn.m
+++ Unscented_KFs/Kinematic/kinematicStateFcn.m
@@ -3,13 +3,15 @@ function [x] = kinematicStateFcn(x)
 % Date: November 5, 2019
 % Summary:  Euler integration of continuous-time
 %           dynamics x'=f(x) w/ sample time dt
+% Dependencies: Kinematic1.m
 
 % Inputs:   xk = states, x[k]
 % Outputs:  xk1 = propagated states, x[k+1]
 
 dt = 0.05; % Size of time step
 
-t = 0; % dummy time variable for Kinematic1
-% x = x + kinematicStateFcnContinuous(x)*dt;
+t = 0; % dummy time variable for Kinematic1 to run in line 16
+
+% Euler integration of kinematics equation
 x = x + Kinematic1(t,x)*dt;
 end
