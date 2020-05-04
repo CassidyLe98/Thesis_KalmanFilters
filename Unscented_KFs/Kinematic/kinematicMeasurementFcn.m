531de719cfd7b11bed83048884d2975232530639
cle@hmc.edu <cle@hmc.edu>
2020-05-03 21:00:05 -0400

    Thorough comments for kinematicMeasurementFcn.m

--- Unscented_KFs/Kinematic/kinematicMeasurementFcn.m
+++ Unscented_KFs/Kinematic/kinematicMeasurementFcn.m
@@ -1,12 +1,14 @@
 function yk = kinematicMeasurementFcn(xk)
 % Author: Cassidy Le
 % Date: November 6, 2019
-% Summary:  Example measurement function for discrete 
-%           time nonlinear state estimators
+% Summary:  Measurement function for discrete time linear state
+%           estimators. Indicates which states are being "measured".
+%           In this case, measuring velocity.
 % Inputs:   xk = states at time k, x[k]
 % Outputs:  yk = y[k], measurements at time k
-%
-% The measurement is the first state with multiplicative noise
 
+% yk = [xk(1); (state: velocity)
+%       xk(2)]; (state: gravity)
+% The measurement is velocity xk(1) with multiplicative noise
 yk = xk(1);
 end
